3395dae6a27f797c7d22f7374ff2c7010692e773
Artem <alekseevwork7@gmail.com>
2024-06-25 17:37:46 +0200

    feat: Add logger in FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -9,6 +10,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -17,31 +19,41 @@ public class FilmController {
 
     @GetMapping
     public Collection<Film> findAll() {
+        log.info("GET findAll Films");
         return films.values();
     }
 
     @PostMapping
     public Film create(@RequestBody Film film) {
+        log.info("Use POST: Film create");
         if (film.getName() == null || film.getName().isBlank()) {
+            log.debug("POST ERROR: create Film - Name is empty");
             throw new ValidationException("Название не может быть пустым");
         }
         if (film.getDescription().length() > 200) {
+            log.debug("POST ERROR: create Film - Description > 200 symbols");
             throw new ValidationException("Максимальная длина описания — 200 символов");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.debug("POST ERROR: create Film - ReleaseDate is before 28-12-1895");
             throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
         }
         if (film.getDuration() < 0) {
+            log.debug("POST ERROR: create Film - Duration < 0");
             throw new ValidationException("продолжительность фильма должна быть положительным числом");
         }
         film.setId(getNextId());
+        log.trace("film set Id");
         films.put(film.getId(), film);
+        log.trace("film put in Map");
         return film;
     }
 
     @PutMapping
     public Film update(@RequestBody Film newFilm) {
+        log.info("Use PUT Film update");
         if (newFilm.getId() == null) {
+            log.debug("PUT ERROR: Id is null");
             throw new ValidationException("Id должен быть указан");
         }
         if (films.containsKey(newFilm.getId())) {
@@ -67,6 +79,7 @@ public class FilmController {
 
 
     private long getNextId() {
+        log.info("Use getNextId for Film");
         long currentMaxId = films.keySet()
                 .stream()
                 .mapToLong(id -> id)
